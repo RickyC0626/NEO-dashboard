5d78ba864319f5f52bd84c61698bce2f705780b6
Ricky Chon <rickychon99@gmail.com>
2022-03-09 23:28:16 -0500

    Remove old API and update READMEs

--- old_api/neo/detector/urls.py
+++
@@ -1,9 +0,0 @@
-  
-from django.urls import path
-from .views import browse
-from .views import news
-
-urlpatterns = [
-    path("browse/", browse, name="browse"),
-    path("news/", news, name="news"),
-]
