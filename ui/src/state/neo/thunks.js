1cb964b2869e28398a6bb55e3784111c2fdcc1b5
Ricky Chon <rickychon99@gmail.com>
2022-03-10 17:46:28 -0500

    Fix URLs for proxy and thunks

--- ui/src/state/neo/thunks.js
+++ ui/src/state/neo/thunks.js
@@ -5,14 +5,14 @@ import { fetchBrowsedNeos, fetchBrowsedNeosReverse } from "./actions";
 // THUNK CREATORS
 export const fetchBrowsedNeosThunk = (page) => (dispatch) =>
 {
-    return axios.get(`/api/browse?page=${page}`)
-    .then((res) => dispatch(fetchBrowsedNeos(res.data.neos)))
+    return axios.get(`/api/v1/browse?page=${page}`)
+    .then((res) => dispatch(fetchBrowsedNeos(res.data)))
     .catch((err) => console.error(err));
 };
 
 export const fetchBrowsedNeosReverseThunk = (page) => (dispatch) =>
 {
-    return axios.get(`/api/browse?page=${page}`)
-    .then((res) => dispatch(fetchBrowsedNeosReverse(res.data.neos)))
+    return axios.get(`/api/v1/browse?page=${page}`)
+    .then((res) => dispatch(fetchBrowsedNeosReverse(res.data)))
     .catch((err) => console.error(err));
 };
