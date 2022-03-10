23aea99b3c79b2dc732f9d74bc2e96fd1b61418c
Ricky Chon <rickychon99@gmail.com>
2022-03-10 17:48:31 -0500

    fix(ui): Update URLs for proxy and thunks (#22)

--- ui/src/state/news/thunks.js
+++ ui/src/state/news/thunks.js
@@ -5,7 +5,7 @@ import { fetchNews } from "./actions";
 // THUNK CREATORS
 export const fetchNewsThunk = () => (dispatch) =>
 {
-    return axios.get(`/api/news`)
+    return axios.get(`/api/v1/news`)
     .then((res) => dispatch(fetchNews(res.data.news)))
     .catch((err) => console.error(err));
 };
