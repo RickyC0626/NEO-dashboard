45e849385d2aaea0a223a16ac79d0cc0a998bd93
Ricky Chon <rickychon99@gmail.com>
2022-07-07 16:25:09 -0400

    Update redux store to use configureStore

--- ui/src/state/shared/store.js
+++ ui/src/state/shared/store.js
@@ -1,19 +1,13 @@
 // Necessities and accessories for constructing our Redux store
-import { combineReducers, applyMiddleware, createStore } from "redux";
-import { createLogger } from "redux-logger";
-import thunkMiddleware from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { combineReducers } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 
 // Individual reducers altogether under an alias
 import * as reducers from "../reducers";
 
 // Construct our Redux store
 const rootReducer = combineReducers(reducers);
-const logger = createLogger({ collapsed: true });
-const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware, logger)
-);
-const store = createStore(rootReducer, middleware);
+const store = configureStore({ reducer: rootReducer });
 
 // Export our store by default, which will be provided to and injected within our entire application
 export default store;
